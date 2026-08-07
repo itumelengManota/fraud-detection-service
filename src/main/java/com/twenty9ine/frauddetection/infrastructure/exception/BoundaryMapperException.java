7212a92bcd53564f5986824682a32623d83e4c20
itumelengManota <itumeleng.manota@gmail.com>
2026-08-07 09:53:08 +0200

    Removed BoundaryMapperException default constructor.

--- src/main/java/com/twenty9ine/frauddetection/infrastructure/exception/BoundaryMapperException.java
+++ src/main/java/com/twenty9ine/frauddetection/infrastructure/exception/BoundaryMapperException.java
@@ -1,9 +1,6 @@
 package com.twenty9ine.frauddetection.infrastructure.exception;
 
 public class BoundaryMapperException extends RuntimeException {
-    public BoundaryMapperException(String message) {
-        super(message);
-    }
     public BoundaryMapperException(String message, Exception exception) {
         super(message, exception);
     }
